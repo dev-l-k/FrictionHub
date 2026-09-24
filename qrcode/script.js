a5eb5f63ebb774d7056c59daf3e4dfd7509149a7
dev-l-k <devlkakkoth545@gmail.com>
2026-09-24 22:36:54 +0530

    debug

--- qrcode/script.js
+++ qrcode/script.js
@@ -11,8 +11,8 @@ function generateQR(){
         text:value,
         width: 220,
         height:220,
-        colorDark: #000000,
-        colorLight: #ffffff,
+        colorDark: "#000000",
+        colorLight: "#ffffff",
         correctLevel: QRCode.CorrectLevel.H
     });
 }
