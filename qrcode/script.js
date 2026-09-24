2cf9537bd61085e97ec33a22d9f7d121cd859e9e
dev-l-k <devlkakkoth545@gmail.com>
2026-09-24 22:12:27 +0530

    Added qr code generator

---
+++ qrcode/script.js
@@ -0,0 +1,35 @@
+const text = document.getElementById("text");
+const qr = document.getElementById("qrcode");
+function generateQR(){
+    let value = text.value.trim();
+    if (value === ""){
+        alert("Enter some text first");
+        return;
+    }
+    qr.innerHTML="";
+    new QRCode(qr,{
+        text:value,
+        width: 220,
+        height:220,
+        colorDark: black,
+        colorLight: white,
+        correctLevel: QRCode.CorrectLevel.H
+    });
+}
+function downloadQR(){
+    let canvas = qr.querySelector("canvas");
+    if(!canvas){
+        alert("No QR Found");
+        return;
+
+    }
+    let link = document.createElement("a");
+    link.href = canvas.toDataURL("image/png");
+    link.download = "frictionhub-qr.png";
+    link.click();
+
+}
+function clearQR(){
+    text.value="";
+    qr.innerHTML="";
+}
